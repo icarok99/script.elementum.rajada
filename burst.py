94ed0d887a5e19ab3c224a20efde7500da4d70ce
addon-rajada <addon.rajada@proton.me>
2024-02-07 13:26:12 -0300

    add action to update providers from repo

--- burst.py
+++ burst.py
@@ -5,8 +5,15 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'resources', 'site-packages'))
 
 from burst.burst import search
+from burst.update_providers import do_update
 from elementum.provider import register
 
+try: arg = sys.argv[1].lower()
+except Exception: arg = None
+
+if arg == "update_providers":
+	do_update()
+	exit()
 
 def search_movie(payload):
     return search(payload, 'movie')
